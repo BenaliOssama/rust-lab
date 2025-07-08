1a524fa313bcbbf4eaa8983408283b4ee40505fa
obenali <oussama.benali@ump.ac.ma>
2025-07-08 22:23:18 +0100

    add condition in case not 2 arguments

---
+++ minigrep/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("not enough arguments");
+        return ;
+    }
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+}
